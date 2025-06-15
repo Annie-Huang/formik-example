af76a3dbe40c86e6cf31ea7e98b954cd6afcb3f2
Annie Huang <annie.hn@gmail.com>
2025-06-15 15:41:23 +1000

    Put the initial template into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello monsterlessons</h1>
+      <form>
+        <div className='field'>
+          <input name='email' placeholder='Email' />
+          <div className='error'></div>
+        </div>
+        <div className='field'>
+          <input name='username' placeholder='Username' />
+          <div className='error'></div>
+        </div>
+        <div className='field'>
+          <input name='password' placeholder='Password' />
+          <div className='error'></div>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
